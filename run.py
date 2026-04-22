9a226f2660637c35ed78a38745817efec817e1a3
Justin <justin.gutierrez@unl.edu.ec>
2026-04-22 09:36:16 -0500

    Fix Gunicorn app shadow conflict and initialize DB externally

--- run.py
+++ run.py
@@ -5,8 +5,9 @@ import os
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+    crear_datos_demo()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        crear_datos_demo()
     app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
